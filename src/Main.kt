2d3b72fbae05b546eb383fbc3313ebcdd17eca60
JoseAntonio <jdiabus511@g.educaand.es>
2025-02-03 20:49:00 +0100

    añadidos de funciones y ajustes

--- src/Main.kt
+++ src/Main.kt
@@ -2,5 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val pila = Pila<Int?>("Numeros enteros", 1, 5, 77, 89)
+    val pila2 = Pila<Any>("Cajon desastre", listOf("123", null, "holi", 10.0, 'c',44), patron = "^[a-z]+$")
 
+    for (elemento in 1..pila.size()) {
+        println(pila.pop())
+    }
+    println(pila)
+    println(pila2)
 }
--- src/Pila.kt
+++ src/Pila.kt
@@ -1,22 +1,27 @@
-class Pila<T>(val desc: String) {
+class Pila<T>(val desc: String, private val patron: String = "") {
 
     private val elementos = mutableListOf<T>()
 
-    constructor(desc: String, vararg elementos: T): this(desc){
-        for (elemento in elementos){
-            this.elementos.add(elemento)
-        }
-        //this.elementos.addAll(elementos)
+    constructor(desc: String, vararg elementos: T, patron: String = ""): this(desc, patron){
+        addElements(elementos.toList())
+    }
+
+    constructor(desc: String, elementos: List<T>, patron: String = ""): this(desc, patron){
+        addElements(elementos)
     }
 
 
-    private fun size() = elementos.size
+    fun size() = elementos.size
 
     private fun isEmpty() = size() == 0
 
+    private fun addElements(elementos: List<T>){
+        for (elemento in elementos){
+            push(elemento)
+        }
+    }
 
     fun pop(): T?{
-
         if (!isEmpty()) {
             val ultimoElemento = elementos[size() - 1]
             elementos.removeAt(size() - 1)
@@ -25,11 +30,27 @@ class Pila<T>(val desc: String) {
         return null
     }
 
-    fun push(elemento: T?): Boolean{
-        if (elemento != null){
+
+    fun checkElement(elemento: T?): Boolean{
+        if (elemento == null) return false
+        if ((elemento !is String) || (Regex(patron).matches(elemento))) return true
+        return false
+    }
+
+
+    fun push(elemento: T): Boolean{
+        if (checkElement(elemento)){
             elementos.add(elemento)
             return true
         }
         return false
+    }
+
+    override fun toString(): String {
+        return if (!isEmpty()){
+            "Pila(\"$desc\", elementos = ${elementos.reversed().joinToString { " - " }})"
+        }else{
+            "Pila(\"$desc\", **Vacía**)"
+        }
     }
 }
